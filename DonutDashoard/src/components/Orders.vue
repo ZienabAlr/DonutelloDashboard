c435bc83d4790142ae61bf70b72e0cebbeb136d1
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-04 16:56:28 +0200

    feat(Order.vue): order content added

--- DonutDashoard/src/components/Orders.vue
+++ DonutDashoard/src/components/Orders.vue
@@ -1,10 +1,20 @@
 <script setup>
-
+import { ref } from 'vue'
 
 </script>
 
 <template>
-
+<div class="order">
+    <img src="../assets/donutTestimg.jpeg" alt="">
+    <h3>Donut name</h3>
+    <p class="client"> Client name</p>
+    <p class="status">status</p>
+    <!-- detail button  -->
+    <button>Details</button>
+    <!-- delete button  -->
+    <button>Delete</button>
+  
+</div>
 
 </template>
 
