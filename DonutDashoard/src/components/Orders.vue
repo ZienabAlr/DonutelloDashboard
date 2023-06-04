3230f250e26186ecfc1b4c01da5b29133f2a6880
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-04 17:57:51 +0200

    style(Orders.vue): style added to h3

--- DonutDashoard/src/components/Orders.vue
+++ DonutDashoard/src/components/Orders.vue
@@ -39,8 +39,13 @@ import { ref } from 'vue'
     width: 100px;
     height: 100px;
     border-radius: 50%;
-    
 }
 
+.order h3 {
+    text-align: center;
+    font-size: 1.5rem;
+    margin: 0.5rem 0;
+    color: #333;
+}
 
 </style>
