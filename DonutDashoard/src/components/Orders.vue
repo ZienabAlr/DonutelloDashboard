cbea390972293af49b1b58ae2329857e2679b5b5
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-04 17:54:34 +0200

    style(Orders.vue): style added to the order card background

--- DonutDashoard/src/components/Orders.vue
+++ DonutDashoard/src/components/Orders.vue
@@ -4,20 +4,32 @@ import { ref } from 'vue'
 </script>
 
 <template>
+
 <div class="order">
     <img src="../assets/donutTestimg.jpeg" alt="">
     <h3>Donut name</h3>
     <p class="client"> Client name</p>
-    <p class="status">status</p>
+    <p class="status">status: </p>
     <!-- detail button  -->
-    <button>Details</button>
+    <button class=" details">Details</button>
     <!-- delete button  -->
-    <button>Delete</button>
+    <button class="delete">Delete</button>
   
 </div>
 
 </template>
 
 <style scoped>
+.order {
+   
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    padding: 0.5rem;
+    margin: 5rem 1rem;
+    border-radius: 0.5rem;
+    width: 200px;
+    height: 300px;
+    background: #fff;
+    
+}
 
 </style>
